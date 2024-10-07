f311f46cc2a1c004efb66161019276413b0f4018
drohit02 <drohit045@gmail.com>
2024-10-08 00:14:01 +0530

    RESTful Web-Service initial stage

---
+++ REstFul-WebServices/src/main/java/com/restapi/webservices/REstFulWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.restapi.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class REstFulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(REstFulWebServicesApplication.class, args);
+	}
+
+}

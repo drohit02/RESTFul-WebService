4b1b55d0708e662621b2c58695cdfdc7b03ee683
drohit02 <drohit045@gmail.com>
2024-10-12 23:55:47 +0530

    Location Code to returns the URI path for created resource

--- REstFul-WebServices/src/main/java/com/restapi/webservices/controller/UserController.java
+++ REstFul-WebServices/src/main/java/com/restapi/webservices/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.restapi.webservices.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.restapi.webservices.dto.UserDTO;
 import com.restapi.webservices.models.User;
@@ -31,15 +33,21 @@ public class UserController {
 	@PostMapping("/users")
 	public ResponseEntity<User> persistUser(@RequestBody UserDTO user) {
 		/*
-		 * 1. Created method to saved user 2.first mapped the method with respective
-		 * HTTP method i.e.POST 3.Accept the data from request body and saved in UserDTO
-		 * object 4.Send this data or object to the dao to saved in database 5.If the
-		 * data saved properly in database it returns the saved object 6.Saved object
-		 * will be type of the User and the response is given back to User.
+		 * 1. Created method to saved user 
+		 * 2.first mapped the method with respective HTTP method i.e.POST 
+		 * 3.Accept the data from request body and saved in UserDTO object 
+		 * 4.Send this data or object to the dao to saved in database 
+		 * 5.If the data saved properly in database it returns the saved object 
+		 * 6.Saved object will be type of the User and the response is given back to User.
+		 * 7.In the response location to check the saved data present for consumer
 		 */
 
 		User savedUser = this.userService.saveUser(user);
-		return ResponseEntity.status(HttpStatus.OK).body(savedUser);
+		URI location = ServletUriComponentsBuilder.fromCurrentContextPath()
+													.path("/{id}")
+													.buildAndExpand(savedUser.getUseId())
+													.toUri();
+		return ResponseEntity.created(location).body(savedUser);
 
 	}
 
